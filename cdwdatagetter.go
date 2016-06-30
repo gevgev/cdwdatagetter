6aa0f2d390f189440cf008486996ccf743ab023f
gevgev <gevgev@yahoo.com>
2016-06-29 18:19:46 -0700

    Mso list processing added, looping through mso list to get the lsit of objects

--- cdwdatagetter.go
+++ cdwdatagetter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
@@ -23,11 +24,13 @@ const (
 )
 
 var (
-	regionName string
-	bucketName string
-	date       string
-	verbose    bool
-	appName    string
+	regionName      string
+	bucketName      string
+	date            string
+	msoListFilename string
+
+	verbose bool
+	appName string
 )
 
 func init() {
@@ -35,6 +38,7 @@ func init() {
 	flagRegion := flag.String("r", "us-east-1", "`AWS Region`")
 	flagBucket := flag.String("b", "rovi-cdw", "`Bucket name`")
 	flagDate := flag.String("d", formatDefaultDate(), "`Date`")
+	flagMsoFileName := flag.String("m", "mso-list.csv", "Filename for `MSO` list")
 	flagVerbose := flag.Bool("v", true, "`Verbose`: outputs to the screen")
 
 	flag.Parse()
@@ -42,6 +46,7 @@ func init() {
 		regionName = *flagRegion
 		bucketName = *flagBucket
 		date = *flagDate
+		msoListFilename = *flagMsoFileName
 		verbose = *flagVerbose
 		appName = os.Args[0]
 	} else {
@@ -53,19 +58,49 @@ func init() {
 func usage() {
 	fmt.Printf("%s, ver. %s\n", appName, version)
 	fmt.Println("Command line:")
-	fmt.Printf("\tprompt$>%s -r <aws_region> -b <s3_bucket_name> -d <date> \n", appName)
+	fmt.Printf("\tprompt$>%s -r <aws_region> -b <s3_bucket_name> -d <date> -m <mso-list-file-name>\n", appName)
 	flag.Usage()
 	os.Exit(-1)
 }
 
 func PrintParams() {
-	log.Printf("Provided: -r: %s, -b: %s, -d: %v, -v: %v\n",
+	log.Printf("Provided: -r: %s, -b: %s, -d: %v, -m %s, -v: %v\n",
 		regionName,
 		bucketName,
 		date,
+		msoListFilename,
 		verbose,
 	)
 
+}
+
+type MsoType struct {
+	Code string
+	Name string
+}
+
+func getMsoNamesList() []MsoType {
+	msoList := []MsoType{}
+
+	msoFile, err := os.Open(msoListFilename)
+	if err != nil {
+		log.Fatalf("Could not open Mso List file: %s, Error: %s\n", msoListFilename, err)
+	}
+
+	r := csv.NewReader(msoFile)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Could not read MSO file: %s, Error: %s\n", msoListFilename, err)
+	}
+
+	for _, record := range records {
+		msoList = append(msoList, MsoType{record[0], record[1]})
+	}
+	return msoList
+}
+
+func formatPrefix(path, msoCode string) string {
+	return fmt.Sprintf("%s/%s/delta/", path, msoCode)
 }
 
 func main() {
@@ -73,34 +108,45 @@ func main() {
 	if verbose {
 		PrintParams()
 	}
+
 	session := session.New(&aws.Config{
 		Region: aws.String(regionName),
 	})
 
 	svc := s3.New(session)
 
-	params := &s3.ListObjectsInput{
-		Bucket: aws.String(bucketName),
-	}
+	msoList := getMsoNamesList()
 
-	resp, err := svc.ListObjects(params)
-	if err != nil {
-		log.Println("Failed to list objects: ", err)
-		os.Exit(-1)
-	}
+	for _, mso := range msoList {
+		prefix := formatPrefix("event/tv_viewership", mso.Code)
+		if verbose {
+			log.Println("Prefix: ", prefix)
+		}
+		params := &s3.ListObjectsInput{
+			Bucket: aws.String(bucketName),
+			Prefix: aws.String(prefix),
+		}
 
-	log.Println("Buckets:")
-	for _, key := range resp.Contents {
-		log.Printf(*key.Key)
-	}
+		resp, err := svc.ListObjects(params)
+		if err != nil {
+			log.Println("Failed to list objects: ", err)
+			os.Exit(-1)
+		}
 
-	filesList := filterObjectsByDate()
+		log.Println("Number of objects: ", len(resp.Contents))
+		log.Println("Files:")
+		for _, key := range resp.Contents {
+			log.Printf(*key.Key)
+			//			log.Println("Downloading: ", file)
+		}
 
-	for file := range filesList {
-		log.Println("Downloading: ", file)
 	}
 }
 
-func filterObjectsByDate() []string {
-	return []string{}
+func filterObjectsByDate(date string) []string {
+
+	filteredFileNames := []string{}
+
+	return filteredFileNames
+
 }
